9f258a8d776e0101435c241d4b3a26a38c0a2772
Darbetigh <mustafaakgul@yandex.com>
2019-02-14 18:29:40 +0300

    Initial commit

---
+++ src/main/java/com/example/restfulserviceusing/RestfulserviceusingApplication.java
@@ -0,0 +1,14 @@
+package com.example.restfulserviceusing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulserviceusingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulserviceusingApplication.class, args);
+    }
+
+}
+
